9161d4e00c2bb2d03b3f2978d6d2fa8295499a28
surajverma2587 <surajverma2587@gmail.com>
2024-10-21 11:31:31 +0100

    get all and get by id

--- src/controllers/studentsController.js
+++ src/controllers/studentsController.js
@@ -1,9 +1,49 @@
-const getStudents = (req, res) => {
-  return res.send("getStudents");
+const pool = require("../config/db");
+
+const getStudents = async (req, res) => {
+  try {
+    // use pool to get data from DB
+    const [rows] = await pool.query("SELECT * FROM students");
+
+    return res.json({
+      data: rows,
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    return res.status(500).json({
+      error: "Failed to get all students",
+    });
+  }
 };
 
-const getStudent = (req, res) => {
-  return res.send("getStudent");
+const getStudent = async (req, res) => {
+  try {
+    // get id from req
+    const { id } = req.params;
+
+    // use pool to get data from DB
+    const [rows] = await pool.query("SELECT * FROM students WHERE id = ?", [
+      id,
+    ]);
+
+    // check if exits
+    if (rows.length === 0) {
+      return res.status(404).json({
+        error: "Student not found",
+      });
+    }
+
+    return res.json({
+      data: rows[0],
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    return res.status(500).json({
+      error: "Failed to get all students",
+    });
+  }
 };
 
 const createStudent = (req, res) => {
